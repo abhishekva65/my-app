f7fe167afafa49c853a91c07edc6ac1203ec040c
abhishekba65 <abhishekva65@gmail.com>
2022-05-02 18:28:28 +0530

    Updates

---
+++ precache-manifest.d7c5eb77b4b5872cb9eded47955ce848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3146c996ef0acea9ef7",
+    "url": "/my-app/static/css/main.8b0e8be5.chunk.css"
+  },
+  {
+    "revision": "c3146c996ef0acea9ef7",
+    "url": "/my-app/static/js/main.c3146c99.chunk.js"
+  },
+  {
+    "revision": "c0c2bb2e3d4ef3d0d371",
+    "url": "/my-app/static/css/1.a3ce6b65.chunk.css"
+  },
+  {
+    "revision": "c0c2bb2e3d4ef3d0d371",
+    "url": "/my-app/static/js/1.c0c2bb2e.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/my-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "92a279383dc1de7b86d117793fa02fd7",
+    "url": "/my-app/index.html"
+  }
+];
